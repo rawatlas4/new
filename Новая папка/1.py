24385d3f130de54c3cca9758262b395307630d93
rawatlas4 <yarik2021top2@gmail.com>
2025-05-17 19:03:56 +0300

    Update 1.py

--- Новая папка/1.py
+++ Новая папка/1.py
@@ -6,7 +6,17 @@ win_height = 700
 window = pygame.display.set_mode((win_width, win_height))
 clock = pygame.time.Clock()
 with open("2","r",) as file:
-    level1 = file.read()
+    level1 = file.readlines()
+class Pryamokytnuk():
+    def __init__(self,x,y,width,height,color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.rec = pygame.Rect(self.x,self.y,self.width,self.height)
+    def risovka(self):
+        pygame.draw.rect(window,self.color,self.rec)
 class Ball():
     def __init__(self,x,y,color,radius):
         self.x = x
@@ -28,9 +38,17 @@ class Ball():
             self.speed_y*=-1
         if self.x < 0:
             self.speed_x*=-1
+a = []
+for row,line in enumerate(level1):
+    for index,char in enumerate(line):
+        if char == '#':
+            pygame.draw.rect(window,(255,0,0),(index*100,500,70,50))
+            block1 = Pryamokytnuk(index*100,row*100,52,25,(52,78,124))
+            a.append(block1)
+
 
 ball = Ball(10,200,(0,0,255),10)
-enemy = Ball(150,210,(0,255,0),35)
+enemy = Ball(650,310,(0,255,0),35)
 game = 1
 while game:
     window.fill((0,0,0))
@@ -41,12 +59,13 @@ while game:
     ball.dvizhenie()
     enemy.risovka()
     enemy.dvizhenie()
-    for index,char in enumerate(level1):
-        if char == '#':
-            pygame.draw.rect(window,(255,0,0),(index*100,500,70,50))
+    pygame.draw.line(window,(255,255,0),(0,0),(0,700),10)
+    pygame.draw.line(window,(0,255,255),(0,690),(1200,700),10)
+    pygame.draw.line(window,(0,255,0),(1199,699),(1199,0),10)
+    pygame.draw.line(window,(0,255,0),(1199,0),(0,0),10)
 
-
-
+    for i in a:
+        i.risovka()
 
     pygame.display.update()
     clock.tick(60)
